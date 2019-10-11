eb336428587f325445b7422960e5e5ed508b112c
MCoyne1234 <32471492+MCoyne1234@users.noreply.github.com>
2019-10-11 11:51:15 -0700

    Keypad and LCD work. Just need to set up slave light pattern.

--- Labs/lab03/mcoyn001_xfern001_lab03_1/mcoyn001_xfern001_lab03_1/Debug/main.d
+++ Labs/lab03/mcoyn001_xfern001_lab03_1/mcoyn001_xfern001_lab03_1/Debug/main.d
@@ -15,11 +15,14 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
- ../../../../../headers/scheduler.h ../../../../../headers/io.c \
+ ../../../../../headers/scheduler.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ ../../../../../headers/io.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- ../../../../../headers/io.h ../../../../../headers/spi.h
+ ../../../../../headers/io.h ../../../../../headers/spi.h \
+ ../../../../../headers/keypad.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -57,6 +60,8 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 
 ../../../../../headers/scheduler.h:
 
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
 ../../../../../headers/io.c:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
@@ -68,3 +73,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 ../../../../../headers/io.h:
 
 ../../../../../headers/spi.h:
+
+../../../../../headers/keypad.h:
--- Labs/lab03/mcoyn001_xfern001_lab03_1/mcoyn001_xfern001_lab03_1/main.c
+++ Labs/lab03/mcoyn001_xfern001_lab03_1/mcoyn001_xfern001_lab03_1/main.c
@@ -19,15 +19,34 @@
 #include <timer.h>
 #include <stdio.h>
 #include <scheduler.h>
+#include <string.h>
 #include "io.c"
 #include "spi.h"
-
+#include "keypad.h"
 
 enum States{ START, LISTEN, SEND };
 enum States2{ START2, RECIEVE };
 enum States3{ START3, ON};
-char data, dataRec, master, mask;
+char data, key, master, maskUp, maskLow;
+char pattern, p_size, speed, slave, t_count, t_max; 
+int iter;
 
+char p1[2] = {0xF0, 0x0F};
+char p2[2] = {0xAA, 0x55};
+char p3[14] = {128,64,32,16,8,4,2,1,2,4,8,16,32,64};
+char p4[18] = {0,1,3,7,15,31,63,127,255,127,63,31,15,7,5,3,1,0};        
+
+char *paterns[4] = {p1,p2,p3,p4};   
+char sizes[4] = {2,2,14,17}; 
+char speeds[] = {40,20,10,5,2,1};
+    
+char message_1[9] = "Ptrn: 0 ";
+char message_2[7] = "Spd: 0";
+char message_3[6] = "uC: 0";
+
+
+void keyAssign();
+void slaveData();
 
 int Tick(int state){
     
@@ -89,11 +108,15 @@ int Tick3(int state){
         case START3:
         break;
         case ON:
-            if(master){
-                if(data == 0x10) data = 0x00;
-                else data = 0x10;                               
+            if(master) {
+                key = GetKeypadKey();
+                if(key != '\0' ){
+                    keyAssign();
+                 }
+            }else  slaveData();           
+            /*if(!master){                                   
                 PORTA = ( (PORTA & ~mask) | ( (data) & mask) );
-            }
+            }*/               
         break;
         default:
         break;
@@ -104,7 +127,50 @@ int Tick3(int state){
             state = ON;
         break;
         case ON:
-        //state = OFF;
+        break;
+        default:break;
+    }
+    return state;
+}
+
+int Tick4(int state){
+    
+    switch(state){
+        case START3:
+            pattern =0; 
+            p_size = 0; 
+            speed=0; 
+            slave = 0; 
+            t_count=0 ;
+            t_max=0;
+            iter = 0;
+            if(master) key = 10;
+        break;
+        case ON:
+            if(t_count == t_max){
+                if(master && (key != '\0' )) {
+                    message_1[6] = (pattern +'1');
+                    message_2[5] = (speed +'1');
+                    message_3[4] = (slave +'0');
+                    char line_1[17] = {};
+                    strcat(line_1, message_1);
+                    strcat(line_1, message_2);
+                     
+                    LCD_DisplayString( 1, (unsigned char*)line_1);
+                    //LCD_DisplayString( 17, (unsigned char*)message_2);
+                    t_count = 0;
+                }
+            }else ++t_count;                
+        break;
+        default:
+        break;
+    }
+    
+    switch(state){
+        case START:
+        state = ON;
+        break;
+        case ON:
         break;
         default:break;
     }
@@ -118,19 +184,24 @@ int main(void)
     DDRC = 0xF0; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
     
-    TimerSet(2);
+    TimerSet(50);
     TimerOn();
     
-    //sei();
+    LCD_init();
+    LCD_ClearScreen();
+    //LCD_Cursor(1);   
+    //LCD_DisplayString( 1, (unsigned char*)"YO");
     
-    mask = 0x10;
+    maskUp = 0xF0;
+    maskLow = 0x0F;
     
-    unsigned long t1 = 50;
-    unsigned long t2 = 3;
-    unsigned long t3 = 250;
+    unsigned long t1 = 6;
+    unsigned long t2 = 2;
+    unsigned long t3 = 1;
+    unsigned long t4 = 1;
     
-    static task task1, task2, task3;
-    task *tasks[] = { &task3, &task1, &task2};
+    static task task1, task2, task3, task4;
+    task *tasks[] = { &task3, &task1, &task2, &task4};
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
     
     task1.state = 0;//Task initial state.
@@ -148,8 +219,12 @@ int main(void)
     task3.elapsedTime = t3; // Task current elapsed time.
     task3.TickFct = &Tick3; // Function pointer for the tick.
     
+    task4.state = 0;//Task initial state.
+    task4.period = t4;//Task Period.
+    task4.elapsedTime = t4; // Task current elapsed time.
+    task4.TickFct = &Tick4; // Function pointer for the tick.
+    
     data = 0;
-    dataRec = 0;
     
     if(PINB & 0x01) master = 0xFF;
     else master = 0x00;
@@ -177,3 +252,62 @@ int main(void)
     return 0;
 }
 
+void keyAssign(){
+    
+    switch(key){
+        case 'A':
+            pattern = 0;
+            p_size = 2;
+        break;
+        case 'B':
+            pattern = 1;
+        p_size = 2;
+        break;
+        case 'C':
+            pattern = 2;
+        p_size = 14;
+        break;
+        case 'D':
+            pattern = 3;
+            p_size = 17;
+        break;
+        case '1':
+            speed = 0;
+        break;
+        case '2':
+            speed = 1;
+        break;
+            case '3':
+            speed = 2;
+        break;
+        case '4':
+            speed = 3;
+        break;
+        case '5':
+            speed = 4;
+        break;
+        case '6':
+            speed = 5;
+        break;
+        case '7':
+            slave = 0;
+        break;
+        case '8':
+            slave = 1;
+        break;
+        case '9':
+            slave = 2;
+        break;
+        default:
+        break;
+    }
+    
+    data = ( (data & ~maskUp) | ( (pattern << 4) & maskUp) )
+    | ( ( data & ~maskLow) | ( (speed) & maskLow) );
+}
+
+void slaveData(){ 
+    pattern = (data >> 4);
+    speed = (data & 0x0F);
+    p_size = sizes[data];
+}
