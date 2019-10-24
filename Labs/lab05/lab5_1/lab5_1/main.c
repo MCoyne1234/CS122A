7e7e668554d1461f20ad1c5b4477691a2aba2f91
MCoyne1234 <32471492+MCoyne1234@users.noreply.github.com>
2019-10-23 17:37:21 -0700

    updated shift reg header

--- Labs/lab05/lab5_1/lab5_1/main.c
+++ Labs/lab05/lab5_1/lab5_1/main.c
@@ -1,17 +1,143 @@
 /*
- * lab5_1.c
+ * mcoyn001_lab01.c
  *
- * Created: 10/23/2019 2:57:55 PM
+ * Created: 10/2/2019 3:23:30 PM
  * Author : Administrator
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <bit.h>
+#include <timer.h>
+#include <stdio.h>
+#include <scheduler.h>
+#include "shift_reg.h"
 
+ enum States{ START, ONE, TWO, THREE};
+ enum States2{ START2, ON, OFF};
+ int button; 
+
+ int Tick(int state){
+     
+     switch(state){
+         case START:
+            PORTB = 0x00;
+         break;
+         case ONE:
+            PORTB = PORTB & 0xF8;
+            PORTB = PORTB | 0x01;
+         break;
+         case TWO:
+            PORTB = PORTB & 0xF8;
+            PORTB = PORTB | 0x02;
+         break;
+         case THREE:
+            PORTB = PORTB & 0xF8;
+            PORTB = PORTB | 0x04;
+         break;
+         default:
+         break;
+     }
+     
+     switch(state){
+         case START:
+            state = ONE;
+         break;
+         case ONE:
+            state = TWO;
+         break;
+         case TWO:
+            state = THREE;
+         break;
+         case THREE:
+            state = START;
+         break;
+         default:break;
+     }
+     return state;
+ }     
+ 
+  int Tick2(int state){
+      
+      switch(state){
+          case START2:
+          PORTB = 0x00;
+          break;
+          case ON:
+          PORTB = PORTB | 0x08;
+          break;
+          case OFF:
+          PORTB = PORTB & 0xF7;
+          break;
+          default:
+          break;
+      }
+      
+      switch(state){
+          case START2:
+          state = ON;
+          break;
+          case ON:
+          state = OFF;
+          break;
+          case OFF:
+          state = ON;
+          break;
+          default:break;
+      }
+      return state;
+  }
 int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
+{   
+   DDRA = 0x00; PORTA = 0x00;
+   DDRB = 0xFF; PORTB = 0x00;
+    
+    TimerSet(500);
+    TimerOn();
+        
+    unsigned long t1 = 1;
+    unsigned long t2 = 2;
+    
+    static task task1, task2;
+    task *tasks[] = { &task1, &task2};
+    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+    
+    task1.state = 0;//Task initial state.
+    task1.period = t1;//Task Period.
+    task1.elapsedTime = t1; // Task current elasped time.
+    task1.TickFct = &Tick; // Function pointer for the tick.
+    
+    task2.state = 0;//Task initial state.
+    task2.period = t2;//Task Period.
+    task2.elapsedTime = t2; // Task current elasped time.
+    task2.TickFct = &Tick2; // Function pointer for the tick.
+    
+    char butt = 0;
+    unsigned short i; // Scheduler for-loop iterator
+    while(1){
+        // Scheduler code
+        for ( i = 0; i < numTasks; i++ ) {
+            // Task is ready to tick
+            if ( tasks[i]->elapsedTime == tasks[i]->period ) {
+                // Setting next state for task
+                tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+                // Reset the elapsed time for next tick.
+                tasks[i]->elapsedTime = 0;
+            }
+            tasks[i]->elapsedTime += 1;
+        }
+        while(!TimerFlag){
+            butt = ( !(PINA & 0x04) );
+            if( butt){
+                button = 1;
+                while(button == 1 ){
+                    butt = ( !(PINA & 0x04) );
+                    if( butt ) button = 0;
+                }
+            }
+        };
+        TimerFlag = 0;
     }
+       return 0;
 }
 
--- headers/shift_reg.h
+++ headers/shift_reg.h
@@ -1,19 +1,34 @@
 #include <avr/io.h>
 
 #define SHIFT_REG PORTA
-#define SER PORTA4 
-#define RCLK PORTA2
-#define SRCLK PORTA1
-#define SRCLR PORTA0
+#define SER 	  PORTA4 
+#define OE 		  PORTA3
+#define RCLK 	  PORTA2
+#define SRCLK 	  PORTA1
+#define SRCLR 	  PORTA0
+
+#define OE2		  PORTA5
+#define RCLK2	  PORTA6
+#define SRCLR2	  PORTA7
+
+enum disp_state{DISP_OFF, DISP_ON};
+    
+sr_display(unsigned char reg, int state){
+	if(reg = 0){
+		PORTA = (PORTA & ~(1 << OE)) | (state << OE);	
+	}else{
+		PORTA = (PORTA & ~(1 << OE2)) | (state << OE2);
+	}
+}  
 
 void transmit_data(unsigned char data) {
-	// SER (PB0), RCLK (PB1), SRCLK (PB2), SRCLR (PB3)
-	// when ready to begin transmitting, set RCLK (PB1) low and SRCLR (PB3) high
-	// for each bit of data, set SRCLR (PB3) high and RCLK (PB1) Low,
-	// Then for each bit, set SRCLK (PB2) low, then set SER (PB0) to the value of the
-	// bit being transfered (1 or 0), then set SRCLK (PB2) high
-	unsigned char i = 0, bit;
-	unsigned char mask = (1 << SER);
+	// SER, RCLK, SRCLK, SRCLR
+	// when ready to begin transmitting, set RCLK low and SRCLR high
+	// for each bit of data, set SRCLR high and RCLK Low,
+	// Then for each bit, set SRCLK low, then set SER to the value of the
+	// bit being transfered (1 or 0), then set SRCLK high
+	unsigned char i = 0, bit = 0;
+	unsigned char disp = ((SHIFT_REG >> OE) & 0x01) << OE;
 	while(i < 8) {
 		bit = ( (data >> i) & 0x01) << SER;
 		SHIFT_REG = (1 << SRCLR);
@@ -26,6 +41,43 @@ void transmit_data(unsigned char data) {
 	}
 	
 	SHIFT_REG |= (1 << RCLK);
-	SHIFT_REG = 0x00;
+	SHIFT_REG = (0x00 | disp);
+}
+
+void transmit_data_2(unsigned char data, unsigned char reg) {
+	// SER, RCLK, SRCLK, SRCLR
+	// when ready to begin transmitting, set RCLK low and SRCLR high
+	// for each bit of data, set SRCLR high and RCLK Low,
+	// Then for each bit, set SRCLK low, then set SER to the value of the
+	// bit being transfered (1 or 0), then set SRCLK high
+	unsigned char output = SHIFT_REG;
+	unsigned char out_en, reg_clk, sreg_clr;
 	
+	if(!reg){
+		out_en = OE;
+		reg_clk = RCLK;
+		sreg_clr = SRCLR;
+	}else{
+		out_en = OE2;
+		reg_clk = RCLK2;
+		sreg_clr = SRCLR2;
+	}
+	
+	unsigned char i = 0, bit = 0;
+	unsigned char disp = ((output >> out_en) & 0x01) << out_en;
+	while(i < 8) {
+		bit = ( (data >> i) & 0x01) << SER;
+		output = (1 << sreg_clr);
+		//SHIFT_REG |= ((data >> i) & SER);
+		output &= ~(1 << SER);
+		output |= bit;
+		
+		output |= (1 << SRCLK);
+		i++;
+	}
+	
+	output |= (1 << reg_clk);
+	output = (0x00 | disp);
+	
+	SHIFT_REG = output;
 }
